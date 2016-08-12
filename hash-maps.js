19bce2b8e50be27da0d410c46f1dba0837e19b18
Kyle <kyleludlow@gmail.com>
2016-08-11 21:39:34 -0700

    hash map questions answered

--- hash-maps.js
+++ hash-maps.js
@@ -1,94 +1,88 @@
-// var HashMap = function(initialCapacity) {
-//     this.length = 0;
-//     this._slots = [];
-//     this._capacity = initialCapacity || 8;
-//     this._deleted = 0;
-// };
-// HashMap.MAX_LOAD_RATIO = 0.9;
-// HashMap.SIZE_RATIO = 3;
-//
-// HashMap.prototype.get = function(key) {
-//     var index = this._findSlot(key);
-//     if (this._slots[index] === undefined) {
-//         throw new Error('Key error');
-//     }
-//     return this._slots[index].value;
-// };
-//
-// HashMap.prototype.set = function(key, value) {
-//     var loadRatio = (this.length + this._deleted + 1) / this._capacity;
-//     if (loadRatio > HashMap.MAX_LOAD_RATIO) {
-//         this._resize(this._capacity * HashMap.SIZE_RATIO);
-//     }
-//
-//     var index = this._findSlot(key);
-//     this._slots[index] = {
-//         key: key,
-//         value: value,
-//         deleted: false
-//     };
-//     this.length++;
-// };
-//
-// HashMap.prototype.remove = function(key) {
-//     var index = this._findSlot(key);
-//     var slot = this._slots[index];
-//     if (slot === undefined) {
-//         throw new Error('Key error');
-//     }
-//     slot.deleted = true;
-//     this.length--;
-//     this._deleted++;
-// };
-//
-// HashMap.prototype._findSlot = function(key) {
-//     var hash = HashMap._hashString(key);
-//     var start = hash % this._capacity;
-//
-//     for (var i=start; i<start + this._capacity; i++) {
-//         var index = i % this._capacity;
-//         var slot = this._slots[index];
-//         if (slot === undefined || (slot.key == key && !slot.deleted)) {
-//             return index;
-//         }
-//     }
-//     // Unreachable
-// };
-//
-// HashMap.prototype._resize = function(size) {
-//     var oldSlots = this._slots;
-//     this._capacity = size;
-//     this._deleted = 0;
-//     this._slots = [];
-//     for (var i=0; i<oldSlots.length; i++) {
-//         var slot = oldSlots[i];
-//         if (slot !== undefined && !slot.deleted) {
-//             this.set(slot.key, slot.value);
-//         }
-//     }
-//
 
-    // 1. Check whether any permutation of a string is a palindrome.
+// 1. Check whether any permutation of a string is a palindrome.
 
 
-  function isPalindrome(string) {
+function isPalindrome(string) {
 
-    var unpairedCharacters = new Set();
+  // create new hash set for unpaired characters
+  var unpairedCharacters = new Set();
 
-    for (var i = 0; i < string.length; i++){
-      var char = string[i].toLowerCase();
 
-      if (unpairedCharacters.has(char)) {
+  for (var i = 0; i < string.length; i++){
 
-        unpairedCharacters.delete(char);
+    // set current character to variable and make lower case
+    var char = string[i].toLowerCase();
 
-      } else {
+    // if a character is in unpaire character hashset, remove it
+    if (unpairedCharacters.has(char)) {
 
-        unpairedCharacters.add(char);
+      unpairedCharacters.delete(char);
 
-      }
+    // else add character to hash set
+    } else {
+
+      unpairedCharacters.add(char);
+
     }
-    return unpairedCharacters.size <= 1;
   }
 
-  console.log(isPalindrome('admam'));
+  // if there are 0 or 1 unpaired characters, return true, else false
+  return unpairedCharacters.size <= 1;
+}
+
+console.log(isPalindrome('admam'));
+
+
+// 2. group a list of words into anagrams.
+// For example, if the input was
+// ['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race'],
+// the output should be:
+// [['east', 'teas', 'eats'], ['cars', 'arcs'], ['acre', 'race']].
+
+function listAnagrams(strs) {
+
+  // creates an array for resulting arrays
+  var result = [];
+
+  // catch for empty or nonexistent array
+  if(strs === null || strs.length === 0) return result;
+
+  // creates hash map for storing each letter combination
+  var map = {};
+  for (var i = 0; i < strs.length; i++){
+
+      // take string from array, break into array of letters
+      var arr = strs[i].split('');
+
+      // sort array of letters in alphabetical order
+      arr.sort();
+
+      // turn array of letters into string
+      var sortedString = arr.join('');
+
+      // if sortedString not in hash map
+      if(map[sortedString] == null){
+
+          // create array for all anagrams of sortedString
+          // push sortedString to array and set hash key value
+          // to array of anagrams
+          var anagramArray = [];
+          anagramArray.push(strs[i]);
+          map[sortedString] = anagramArray;
+      } else {
+          // else push the current string to anagram array
+          map[sortedString].push(strs[i]);
+      }
+  }
+
+  // for each anagram array in map, push to result array
+  for(var array in map){
+    result.push(map[array]);
+  }
+
+  return result;
+}
+
+var anaWords = ['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race'];
+
+console.log(listAnagrams(anaWords));
